7f8c9fe679dc529e098e7a554a2693614564bbf3
Евгений <jenya.kuzya@mail.ru>
2018-09-29 22:26:30 +0300

    First Classes

---
+++ ClassLibrary/TraceResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace TracerLib
+{
+    public class TraceResult
+    {
+        private ConcurrentDictionary<int, TracedThread> tracedThreads;
+
+        public TraceResult()
+        {
+            tracedThreads = new ConcurrentDictionary<int, TracedThread>();
+        }
+
+        public void AddThread(TracedThread addedThread)
+        {
+            tracedThreads.TryAdd(Thread.CurrentThread.ManagedThreadId, new TracedThread());
+        }
+    }
+}
--- Tracer/Program.cs
+++ Tracer/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Tracer
 {
@@ -10,6 +10,18 @@ namespace Tracer
     {
         static void Main(string[] args)
         {
+            //Tracer tracer = new Tracer();
+            //tracer.start();
+            //for(i = 0, i < 4, i++){
+            //  thread.new();
+            //  waiting of closing threads
+            //}
+            //tracer.stop();
+            //tracer.showResult();
+
+
+
+            //Thread.CurrentThread.ManagedThreadId;
         }
     }
 }
